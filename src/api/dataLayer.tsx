bd883fb835b498d5cb39e02733530a88c115f81f
Hugo Silva <hugoflsilva@gmail.com>
2024-12-18 19:00:32 +0000

    Created the datalayer concept, and added the typing for the json data structures

---
+++ src/api/dataLayer.tsx
@@ -0,0 +1,14 @@
+﻿import { promises as fs } from 'fs';
+import path from 'path'
+import {DataType} from "@/lib/DataTypes.d";
+
+export async function getLocalData() {
+    
+    const filePath = path.join(process.cwd(), 'src/api/data.json');
+    
+    const jsonData = await fs.readFile(filePath, 'utf8');
+
+    const objectData: DataType = JSON.parse(jsonData);
+
+    return objectData
+}
---
+++ src/lib/DataTypes.d.tsx
@@ -0,0 +1,21 @@
+﻿export type ProjectType = {
+    id: number
+    title: string
+    image: string
+    image_alt: string
+    images: {
+        id: number
+        url: string
+    }[]
+    text_intro: string
+    text_dev: string
+    text_link: {
+        url: string
+        label: string
+    }
+    text_tech: string
+}
+
+export type DataType = {
+    projects: ProjectType[]
+}
