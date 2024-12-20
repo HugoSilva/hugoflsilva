09816ad1afad38073d635e98ee2ebeb9b8a6963d
Hugo Silva <hugoflsilva@gmail.com>
2024-12-20 16:27:55 +0000

    Added the motion package, this will replace the animate library

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,4 +1,15 @@
-﻿export default function About() {
+﻿import * as motion from "motion/react-client"
+
+const SlideRight = {
+    visible: {x: 0, opacity: 1, scale: 1},
+    hidden: {x: -100, opacity: 0.5, scale: 0.8},
+}
+const SlideLeft = {
+    visible: {x: 0, opacity: 1, scale: 1},
+    hidden: {x: 100, opacity: 0.5, scale: 0.8},
+}
+
+export default function About() {
     return (
         <section id="about" className="about">
             <div className="section-heading text-center">
@@ -8,52 +19,74 @@
                 <div className="about-content">
                     <div className="row">
                         <div className="col-sm-6">
-                            <div className="single-about-txt">
-                                <h3>
-                                    Hi, I'm a seasoned lead programmer going on 7 years of experience in game 
-                                    development, specializing in UI design and implementation. My objective is to 
-                                    create intuitive and responsive interfaces that blend seamlessly with gameplay 
-                                    experiences.
-                                </h3>
-                                <p>
-                                    I’m passionate about finding creative ways to display information to players, 
-                                    always aiming to elevate the gaming experience. As a team motivator, I focus on 
-                                    driving projects to excellence while fostering collaboration. I’m constantly 
-                                    learning—running a small dev studio allows me to explore different fields of game 
-                                    development, like animation and gameplay, broadening my skill set. I’m also an avid 
-                                    follower of gaming news and pop culture, often being the go-to person for random 
-                                    trivia and industry insights!
-                                </p>
-                            </div>
+                            <motion.div
+                                variants={SlideRight}
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{once: true}}
+                                transition={{
+                                    duration: 0.1,
+                                    ease: "easeOut"
+                                }}>
+                                <div className="single-about-txt">
+                                    <h3>
+                                        Hi, I'm a seasoned lead programmer going on 7 years of experience in game
+                                        development, specializing in UI design and implementation. My objective is to
+                                        create intuitive and responsive interfaces that blend seamlessly with gameplay
+                                        experiences.
+                                    </h3>
+                                    <p>
+                                        I’m passionate about finding creative ways to display information to players,
+                                        always aiming to elevate the gaming experience. As a team motivator, I focus on
+                                        driving projects to excellence while fostering collaboration. I’m constantly
+                                        learning—running a small dev studio allows me to explore different fields of
+                                        game
+                                        development, like animation and gameplay, broadening my skill set. I’m also an
+                                        avid
+                                        follower of gaming news and pop culture, often being the go-to person for random
+                                        trivia and industry insights!
+                                    </p>
+                                </div>
+                            </motion.div>
                         </div>
                         <div className="col-sm-offset-1 col-sm-5">
-                            <div className="single-about-img">
-                                <img src="/images/about/profile_image.jpg" alt="profile_image"/>
-                                <div className="about-list-icon">
-                                    <ul>
-                                        <li>
-                                            <a href="https://www.linkedin.com/in/hugoflsilva">
-                                                <i className="fa fa-linkedin" aria-hidden="true"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.facebook.com/rockslidestudios/">
-                                                <i className="fa fa-facebook" aria-hidden="true"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://x.com/hugoflsilva">
-                                                <i className="fa fa-twitter" aria-hidden="true"></i>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="http://blog.rockslidestudios.co.uk/">
-                                                <i className="fa fa-wordpress" aria-hidden="true"></i>
-                                            </a>
-                                        </li>
-                                    </ul>
+                            <motion.div
+                                variants={SlideLeft}
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{once: true}}
+                                transition={{
+                                    duration: 0.1,
+                                    ease: "easeOut"
+                                }}>
+                                <div className="single-about-img">
+                                    <img src="/images/about/profile_image.jpg" alt="profile_image"/>
+                                    <div className="about-list-icon">
+                                        <ul>
+                                            <li>
+                                                <a href="https://www.linkedin.com/in/hugoflsilva">
+                                                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="https://www.facebook.com/rockslidestudios/">
+                                                    <i className="fa fa-facebook" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="https://x.com/hugoflsilva">
+                                                    <i className="fa fa-twitter" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="http://blog.rockslidestudios.co.uk/">
+                                                    <i className="fa fa-wordpress" aria-hidden="true"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
+                            </motion.div>
                         </div>
                     </div>
                 </div>
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -2,10 +2,16 @@
 import {DataType} from "@/lib/DataTypes.d";
 import {getLocalData} from "@/api/dataLayer";
 import Link from "next/link";
+import * as motion from "motion/react-client"
+
+const ZoomIn = {
+    visible: {opacity: 1, scale: 1},
+    hidden: {opacity: 0.1, scale: 0.5},
+}
 
 export default async function Portfolio() {
 
-    const data:DataType = await getLocalData();
+    const data: DataType = await getLocalData();
 
     return (
         <section id="portfolio" className="portfolio">
@@ -16,26 +22,37 @@ export default async function Portfolio() {
                 <div className="container">
                     <div className="portfolio-content">
                         <div className="isotope">
-                        {
-                            data.projects.map((project) => (
-                            <div key={"project_"+project.id} className="col-sm-4">
+                            {
+                                data.projects.map((project) => (
 
-                                <Link href={"/project/"+project.id} className="portfolio-link" data-target="#exampleModal">
-                                    <div className="item">
-                                        <Image
-                                            src={project.image}
-                                            alt={project.image_alt}
-                                            width={488}
-                                            height={290}
-                                        />
-                                        <div className="isotope-overlay">
-                                            <h1>{project.title}</h1>
-                                        </div>
+                                    <div key={"project_" + project.id} className="col-sm-4">
+                                        <motion.div
+                                            variants={ZoomIn}
+                                            initial="hidden"
+                                            whileInView="visible"
+                                            viewport={{once: true}}
+                                            transition={{
+                                                duration: 0.3,
+                                                ease: "easeOut"
+                                            }}>
+                                            <Link href={"/project/" + project.id} className="portfolio-link"
+                                                  data-target="#exampleModal">
+                                                <div className="item">
+                                                    <Image
+                                                        src={project.image}
+                                                        alt={project.image_alt}
+                                                        width={488}
+                                                        height={290}
+                                                    />
+                                                    <div className="isotope-overlay">
+                                                        <h1>{project.title}</h1>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </motion.div>
                                     </div>
-                                </Link>
-                            </div>
-                            ))
-                        }
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
