b85ce8bec12a9ee630164ae3cee1a8dfc4421755
Hugo Silva <hugoflsilva@gmail.com>
2024-12-20 19:07:45 +0000

    Removed unused CSS files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,6 @@ export default function RootLayout({
             <link rel="stylesheet" href="/css/font-awesome.min.css"/>
             <link rel="stylesheet" href="/css/flaticon.css"/>
             <link rel="stylesheet" href="/css/fontello.css"/>
-            <link rel="stylesheet" href="/css/owl.carousel.min.css"/>
-            <link rel="stylesheet" href="/css/owl.theme.default.min.css"/>
             <link rel="stylesheet" href="/css/bootstrap.min.css"/>
             <link rel="stylesheet" href="/css/bootsnav.css"/>
             <link rel="stylesheet" href="/css/style.css"/>
