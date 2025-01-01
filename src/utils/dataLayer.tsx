1b8d3ca3feb0c9c49a032f3f33d66a8313a0cc59
Hugo Silva <hugoflsilva@gmail.com>
2025-01-01 19:30:25 +0000

    Renamed api folder to the utils folder

--- src/utils/dataLayer.tsx
+++ src/utils/dataLayer.tsx
@@ -1,11 +1,11 @@
-﻿import { promises as fs } from 'fs';
+﻿import {promises as fs} from 'fs';
 import path from 'path'
 import {DataType} from "@/lib/DataTypes.d";
 
 export async function getLocalData() {
-    
-    const filePath = path.join(process.cwd(), 'src/api/data.json');
-    
+
+    const filePath = path.join(process.cwd(), 'src/utils/data.json');
+
     const jsonData = await fs.readFile(filePath, 'utf8');
 
     const objectData: DataType = JSON.parse(jsonData);
