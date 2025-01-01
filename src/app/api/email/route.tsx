a411945d118e827456b6392931d43c4d3c08d8b7
Hugo Silva <hugoflsilva@gmail.com>
2025-01-01 19:34:33 +0000

    Added the form feature to send emails from the website

---
+++ src/app/api/email/route.tsx
@@ -0,0 +1,43 @@
+﻿import {type NextRequest, NextResponse} from 'next/server';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+export async function POST(request: NextRequest) {
+    const {email, name, message} = await request.json();
+
+    const transport = nodemailer.createTransport({
+        host: "hugoflsilva.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.PRIVATE_FORM_EMAIL,
+            pass: process.env.PRIVATE_FORM_EMAIL_PASSWORD,
+        },
+    });
+
+    const mailOptions: Mail.Options = {
+        from: process.env.PRIVATE_FORM_EMAIL,
+        to: process.env.PRIVATE_FORM_EMAIL,
+        subject: `Contact form filled by: ${name} ( ${email} )`,
+        text: message,
+    };
+
+    const sendMailPromise = () =>
+        new Promise<string>((resolve, reject) => {
+            transport.sendMail(mailOptions, function (err) {
+                if (!err) {
+                    resolve('Email sent');
+                } else {
+                    reject(err.message);
+                }
+            });
+        });
+
+    try {
+        await sendMailPromise();
+        return NextResponse.json({message: 'Email sent'});
+    } catch (err) {
+
+        return NextResponse.json({error: err}, {status: 500});
+    }
+}
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,4 +1,33 @@
-﻿export default function Contact() {
+﻿'use client'
+
+import * as motion from "motion/react-client";
+import {useForm} from "react-hook-form";
+import {sendEmail} from "@/utils/sendLayer";
+
+const SlideLeft = {
+    visible: {x: 0, opacity: 1, scale: 1},
+    hidden: {x: 150, opacity: 0.1, scale: 1},
+}
+
+const fadeInUp = {
+    visible: {y: "0%", opacity: 1},
+    hidden: {y: "30px", opacity: 0},
+}
+
+export type FormData = {
+    name: string
+    email: string
+    message: string
+};
+
+export default function Contact() {
+
+    const {register, handleSubmit} = useForm<FormData>();
+
+    function onSubmit(data: FormData) {
+        sendEmail(data);
+    }
+
     return (
         <section id="contact" className="contact">
             <div className="section-heading text-center">
@@ -10,18 +39,18 @@
                         <div className="col-md-offset-1 col-md-5 col-sm-6">
                             <div className="single-contact-box">
                                 <div className="contact-form">
-                                    <form>
+                                    <form onSubmit={handleSubmit(onSubmit)}>
                                         <div className="row">
                                             <div className="col-sm-6 col-xs-12">
                                                 <div className="form-group">
                                                     <input type="text" className="form-control" id="name"
-                                                           placeholder="Name*" name="name"/>
+                                                           placeholder="Name*" {...register("name")} />
                                                 </div>
                                             </div>
                                             <div className="col-sm-6 col-xs-12">
                                                 <div className="form-group">
                                                     <input type="email" className="form-control" id="email"
-                                                           placeholder="Email*" name="email"/>
+                                                           placeholder="Email*" {...register("email")} />
                                                 </div>
                                             </div>
                                         </div>
@@ -37,57 +66,80 @@
                                             <div className="col-sm-12">
                                                 <div className="form-group">
                                                     <textarea className="form-control" rows={8} id="comment"
-                                                              placeholder="Message"></textarea>
+                                                              placeholder="Message" {...register("message")}></textarea>
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="row">
-                                            <div className="col-sm-12">
-                                                <div className="single-contact-btn">
-                                                    <a className="contact-btn" href="#" role="button">submit</a>
+                                        <motion.div
+                                            variants={fadeInUp}
+                                            initial="hidden"
+                                            whileInView="visible"
+                                            viewport={{once: true}}
+                                            transition={{
+                                                delay: 0.2,
+                                                duration: 0.3,
+                                                ease: "easeIn"
+                                            }}>
+                                            <div className="row">
+                                                <div className="col-sm-12">
+                                                    <div className="single-contact-btn">
+                                                        <input className="clear-styling white-color-text" type="submit" value="Submit"/>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </motion.div>
                                     </form>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-offset-1 col-md-5 col-sm-6">
-                            <div className="single-contact-box">
-                                <div className="contact-adress">
-                                    <div className="contact-add-head">
-                                        <h3>Hugo Silva</h3>
-                                        <p>Lead Programmer</p>
+                        <motion.div
+                            variants={SlideLeft}
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{once: true}}
+                            transition={{
+                                delay: 0.2,
+                                duration: 0.3,
+                                ease: "easeIn"
+                            }}>
+                            <div className="col-md-offset-1 col-md-5 col-sm-6">
+                                <div className="single-contact-box">
+                                    <div className="contact-adress">
+                                        <div className="contact-add-head">
+                                            <h3>Hugo Silva</h3>
+                                            <p>Lead Programmer</p>
+                                        </div>
+                                        <div className="contact-add-info">
+                                            <div className="single-contact-add-info">
+                                                <h3>phone</h3>
+                                                <p>(+44) 0776 137 2025</p>
+                                            </div>
+                                            <div className="single-contact-add-info">
+                                                <h3>email</h3>
+                                                <p>hugoflsilva@sapo.pt</p>
+                                            </div>
+                                            <div className="single-contact-add-info">
+                                                <h3>website</h3>
+                                                <p>www.hugoflsilva.com</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="contact-add-info">
-                                        <div className="single-contact-add-info">
-                                            <h3>phone</h3>
-                                            <p>(+44) 0776 137 2025</p>
-                                        </div>
-                                        <div className="single-contact-add-info">
-                                            <h3>email</h3>
-                                            <p>hugoflsilva@sapo.pt</p>
-                                        </div>
-                                        <div className="single-contact-add-info">
-                                            <h3>website</h3>
-                                            <p>www.hugoflsilva.com</p>
-                                        </div>
+                                    <div className="hm-foot-icon">
+                                        <ul>
+                                            <li><a href="https://www.linkedin.com/in/hugoflsilva"><i
+                                                className="fa fa-linkedin"></i></a></li>
+                                            <li><a href="https://www.facebook.com/rockslidestudios/"><i
+                                                className="fa fa-facebook"></i></a></li>
+                                            <li><a href="https://x.com/hugoflsilva"><i
+                                                className="fa fa-twitter"></i></a>
+                                            </li>
+                                            <li><a href="http://blog.rockslidestudios.co.uk/"><i
+                                                className="fa fa-wordpress"></i></a></li>
+                                        </ul>
                                     </div>
-                                </div>
-                                <div className="hm-foot-icon">
-                                    <ul>
-                                        <li><a href="https://www.linkedin.com/in/hugoflsilva"><i
-                                            className="fa fa-linkedin"></i></a></li>
-                                        <li><a href="https://www.facebook.com/rockslidestudios/"><i
-                                            className="fa fa-facebook"></i></a></li>
-                                        <li><a href="https://x.com/hugoflsilva"><i className="fa fa-twitter"></i></a>
-                                        </li>
-                                        <li><a href="http://blog.rockslidestudios.co.uk/"><i
-                                            className="fa fa-wordpress"></i></a></li>
-                                    </ul>
                                 </div>
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
