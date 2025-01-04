800954a2ec728103ef5a1137043bf59b3f234592
Hugo Silva <hugoflsilva@gmail.com>
2025-01-04 20:46:04 +0000

    Fixed text visuals in the terms and privacy modals Fixed build issues related to the modals

--- src/app/modal/privacy/modal.tsx
+++ src/app/modal/privacy/modal.tsx
@@ -25,8 +25,9 @@ export default function PrivacyModal() {
                             <div className="modal-body justify">
                                 <h2>Privacy Policy</h2>
                                 <p>Effective date: December 20th, 2024</p>
-                                <p>Hugo Silva ("us", "we", or "our") operates the https://hugoflsilva.com website
-                                    (hereinafter referred to as the "Service").</p>
+                                <p>Hugo Silva (&quot;us&quot;, &quot;we&quot;, or &quot;our&quot;) operates the
+                                    https://hugoflsilva.com website
+                                    (hereinafter referred to as the &quot;Service&quot;).</p>
                                 <p>This page informs you of our policies regarding the collection, use, and disclosure
                                     of personal data when you use our Service and the choices you have associated with
                                     that data. Our Privacy Policy for Hugo Silva is managed with the help of <a
@@ -43,26 +44,28 @@ export default function PrivacyModal() {
                                 <h3>Types of Data Collected</h3>
                                 <h4>Personal Data</h4>
                                 <p>While using our Service, we may ask you to provide us with certain personally
-                                    identifiable information that can be used to contact or identify you ("Personal
-                                    Data"). Personally identifiable information may include, but is not limited to:</p>
+                                    identifiable information that can be used to contact or identify you (&quot;Personal
+                                    Data&quot;). Personally identifiable information may include, but is not limited
+                                    to:</p>
                                 <ul>
                                     <li>Address, State, Province, ZIP/Postal code, City</li>
                                     <li>Cookies and Usage Data</li>
                                 </ul>
                                 <h4>Usage Data</h4>
-                                <p>We may also collect information on how the Service is accessed and used ("Usage
-                                    Data"). This Usage Data may include information such as your computer's Internet
+                                <p>We may also collect information on how the Service is accessed and used (&quot;Usage
+                                    Data&quot;). This Usage Data may include information such as your computer&apos;s
+                                    Internet
                                     Protocol address (e.g. IP address), browser type, browser version, the pages of our
                                     Service that you visit, the time and date of your visit, the time spent on those
                                     pages, unique device identifiers and other diagnostic data.</p>
                                 <h4>Tracking & Cookies Data</h4>
                                 <p>We use cookies and similar tracking technologies to track the activity on our Service
-                                    and hold certain information.</p>
-                                <p>Cookies are files with small amount of data which may include an anonymous unique
+                                    and hold certain information. Cookies are files with small amount of data which may
+                                    include an anonymous unique
                                     identifier. Cookies are sent to your browser from a website and stored on your
                                     device. Tracking technologies also used are beacons, tags, and scripts to collect
-                                    and track information and to improve and analyze our Service.</p>
-                                <p>You can instruct your browser to refuse all cookies or to indicate when a cookie is
+                                    and track information and to improve and analyze our Service. You can instruct your
+                                    browser to refuse all cookies or to indicate when a cookie is
                                     being sent. However, if you do not accept cookies, you may not be able to use some
                                     portions of our Service.</p>
                                 <p>Examples of Cookies we use:</p>
@@ -76,7 +79,7 @@ export default function PrivacyModal() {
                                         purposes.
                                     </li>
                                 </ul>
-                                <h2>Use of Data</h2>
+                                <h3>Use of Data</h3>
                                 <p>Hugo Silva uses the collected data for various purposes:</p>
                                 <ul>
                                     <li>To provide and maintain the Service</li>
@@ -90,7 +93,7 @@ export default function PrivacyModal() {
                                     <li>To monitor the usage of the Service</li>
                                     <li>To detect, prevent and address technical issues</li>
                                 </ul>
-                                <h2>Transfer Of Data</h2>
+                                <h3>Transfer Of Data</h3>
                                 <p>Your information, including Personal Data, may be transferred to — and maintained on
                                     — computers located outside of your state, province, country or other governmental
                                     jurisdiction where the data protection laws may differ than those from your
@@ -117,16 +120,16 @@ export default function PrivacyModal() {
                                     <li>To protect the personal safety of users of the Service or the public</li>
                                     <li>To protect against legal liability</li>
                                 </ul>
-                                <h2>Security Of Data</h2>
+                                <h3>Security Of Data</h3>
                                 <p>The security of your data is important to us, but remember that no method of
                                     transmission over the Internet, or method of electronic storage is 100% secure.
                                     While we strive to use commercially acceptable means to protect your Personal Data,
                                     we cannot guarantee its absolute security.</p>
-                                <h2>Service Providers</h2>
+                                <h3>Service Providers</h3>
                                 <p>We may employ third party companies and individuals to facilitate our Service
-                                    ("Service Providers"), to provide the Service on our behalf, to perform
-                                    Service-related services or to assist us in analyzing how our Service is used.</p>
-                                <p>These third parties have access to your Personal Data only to perform these tasks on
+                                    (&quot;Service Providers&quot;), to provide the Service on our behalf, to perform
+                                    Service-related services or to assist us in analyzing how our Service is used. These
+                                    third parties have access to your Personal Data only to perform these tasks on
                                     our behalf and are obligated not to disclose or use it for any other purpose.</p>
                                 <h3>Analytics</h3>
                                 <p>We may use third-party Service Providers to monitor and analyze the use of our
@@ -150,32 +153,34 @@ export default function PrivacyModal() {
                                         </p>
                                     </li>
                                 </ul>
-                                <h2>Links To Other Sites</h2>
+                                <h3>Links To Other Sites</h3>
                                 <p>Our Service may contain links to other sites that are not operated by us. If you
-                                    click on a third party link, you will be directed to that third party's site. We
-                                    strongly advise you to review the Privacy Policy of every site you visit.</p>
-                                <p>We have no control over and assume no responsibility for the content, privacy
+                                    click on a third party link, you will be directed to that third party&apos;s site.
+                                    We
+                                    strongly advise you to review the Privacy Policy of every site you visit. We have no
+                                    control over and assume no responsibility for the content, privacy
                                     policies or practices of any third party sites or services.</p>
-                                <h2>Children's Privacy</h2>
-                                <p>Our Service does not address anyone under the age of 18 ("Children").</p>
-                                <p>We do not knowingly collect personally identifiable information from anyone under the
+                                <h3>Children&apos;s Privacy</h3>
+                                <p>Our Service does not address anyone under the age of 18 (&quot;Children&quot;). We do
+                                    not knowingly collect personally identifiable information from anyone under the
                                     age of 18. If you are a parent or guardian and you are aware that your Children has
                                     provided us with Personal Data, please contact us. If we become aware that we have
                                     collected Personal Data from children without verification of parental consent, we
                                     take steps to remove that information from our servers.</p>
-                                <h2>Changes To This Privacy Policy</h2>
+                                <h3>Changes To This Privacy Policy</h3>
                                 <p>We may update our Privacy Policy from time to time. We will notify you of any changes
-                                    by posting the new Privacy Policy on this page.</p>
-                                <p>We will let you know via email and/or a prominent notice on our Service, prior to the
-                                    change becoming effective and update the "effective date" at the top of this Privacy
-                                    Policy.</p>
-                                <p>You are advised to review this Privacy Policy periodically for any changes. Changes
+                                    by posting the new Privacy Policy on this page. We will let you know via email
+                                    and/or a prominent notice on our Service, prior to the
+                                    change becoming effective and update the &quot;effective date&quot; at the top of
+                                    this Privacy
+                                    Policy. You are advised to review this Privacy Policy periodically for any changes.
+                                    Changes
                                     to this Privacy Policy are effective when they are posted on this page.</p>
-                                <h2>Contact Us</h2>
-                                <p>If you have any questions about this Privacy Policy, please contact us:</p>
-                                <ul>
-                                    <li>By email: me@hugoflsilva.com</li>
-                                </ul>
+                                <h3>Contact Us</h3>
+                                <p>If you have any questions about this Privacy Policy, please contact us:
+                                    <br/>
+                                    By email: me@hugoflsilva.com
+                                </p>
                                 <button type="button" className="btn btn-primary" data-dismiss="modal"
                                         onClick={closeModal}>
                                     <i className="fa fa-times"></i> Close Privacy Policy
